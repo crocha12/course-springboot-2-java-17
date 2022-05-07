349e2c35dced7b38b89d5ecb67034eef075525a3
Camilo <camilorocha11@gmail.com>
2022-05-06 22:33:46 -0300

    Project created

---
+++ src/test/java/com/crocha/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crocha.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
